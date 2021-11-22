e90bed755f601f21a989c10e8ebb4464c4a8a12c
Dominique Zeise <d.zei@gmx.de>
2021-11-22 22:29:01 +0100

    adds smith-waterman

--- local_alignment/smith-waterman-v1.0.py
+++ local_alignment/smith-waterman-v1.0.py
@@ -3,15 +3,15 @@
 # Created By  : Dominique Zeise
 # GitHub      : https://github.com/CharliesCodes
 # Created Date: 2021/11/03
-# Version     : 2.0
+# Version     : 1.0
 # © Copyright : 2021 Dominique Zeise
 # =============================================================================
 """
-Needleman-Wunsch Algorithm
+Smith-Waterman Algorithm
 
 
 This module uses dynamic programming to finding the optimal
-alignment of two sequences/ strings of similar length.
+alignment of two sequences/ strings of different lengths.
 Its the optimization version of the algorithm.
 """
 # =============================================================================
@@ -21,14 +21,6 @@ import numpy as np
 MATCH = 1
 MISMATCH = -1
 GAP = -2
-
-
-def initialize_matrix(score_matrix, seq1, seq2):
-    for x in range(len(seq1)):
-        score_matrix[0][x] = x * GAP
-    for y in range(len(seq2)):
-        score_matrix[y][0] = y * GAP
-    return score_matrix
 
 
 def fill_matches(score_matrix, seq1, seq2):
@@ -42,6 +34,7 @@ def recalculate_scorematrix(score_matrix, seq1, seq2):
     for y in range(1, len(seq2)):
         for x in range(1, len(seq1)):
             score_matrix[y][x] = max(
+                0,
                 score_matrix[y-1][x-1] + score_matrix[y][x],
                 score_matrix[y-1][x] + GAP,
                 score_matrix[y][x-1] + GAP
@@ -49,17 +42,24 @@ def recalculate_scorematrix(score_matrix, seq1, seq2):
     return score_matrix
 
 
-def traceback(score_matrix, seq1, seq2):
-    seq1_new, seq2_new = [], []
-    x = len(seq1)-1
-    y = len(seq2)-1
+def find_max_coordinates(score_matrix):
+    result = np.where(score_matrix == np.amax(score_matrix))
+    max_coords = (result[0][0].item(), result[1][0].item())
+    return max_coords
 
-    while x>0 and y>0:
+
+def traceback(score_matrix, seq1, seq2, max_coords):
+    seq1_new, seq2_new = [], []
+    (y, x) = max_coords
+    seq1_new.append(seq1[x])
+    seq2_new.append(seq2[y])
+
+    while score_matrix[y][x] != 0:
         dia = score_matrix[y-1][x-1]
         up = score_matrix[y-1][x]
         left = score_matrix[y][x-1]
         max_points = max(dia, up, left)
-
+        score_matrix[y][x] = 0
         # dia
         if (x > 0 and y > 0 and dia == max_points):
             seq1_new.append(seq1[x-1])
@@ -94,16 +94,16 @@ def output(seq1_new, seq2_new):
 
 
 def main():
-    seq1 = ",CTAC,"
-    seq2 = ",CTC,"
+    seq1 = ",ACGACGACTTACTT"
+    seq2 = ",CGTCT"
 
     score_matrix = np.zeros((len(seq2)) * (len(seq1)),
                             dtype="int16").reshape((len(seq2), len(seq1)))
 
-    score_matrix = initialize_matrix(score_matrix, seq1, seq2)
     score_matrix = fill_matches(score_matrix, seq1, seq2)
     score_matrix = recalculate_scorematrix(score_matrix, seq1, seq2)
-    seq1_new, seq2_new = traceback(score_matrix, seq1, seq2)
+    max_coords = find_max_coordinates(score_matrix)
+    seq1_new, seq2_new = traceback(score_matrix, seq1, seq2, max_coords)
     del score_matrix
     output(seq1_new, seq2_new)
 
